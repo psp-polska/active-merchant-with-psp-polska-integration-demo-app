3962e2b14e80a072b6e3f32367a5083e39f11f8b
Piotr Misiurek <p.misiurek@gmail.com>
2011-04-13 07:34:42 +0200

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PspPolskaDemo::Application.config.secret_token = '17e483a970a435727fd563d416fc6ea39851e270e45ec422f4c5fe0b1bbb8c3525d29371c9a85b960a7e1b6eb0d1168a7f56f1e396dfe5cae42eb78a9e68fd12'
