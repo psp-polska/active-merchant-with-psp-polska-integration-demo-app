7f02674ac590c1392d0b892ade812a5d2d8e63e5
Piotr Misiurek <p.misiurek@gmail.com>
2011-04-20 14:33:45 +0200

    PaymentsController: redirect to PspPolska

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,7 +4,12 @@ class PaymentsController < ApplicationController
   def new
     @psp_polska_request = ActiveMerchant::Billing::Integrations::PspPolska::PspPolskaRequest.new(:action => "sale", :amount => 100, :currency => "EUR", :title => "bzdet", :session_id => "some_session_id", :email => "email@example.com", :first_name => "John", :last_name => "Smith", :client_ip => request.env["HTTP_X_FORWARDED_FOR"] || request.remote_ip)
     res = @psp_polska_request.send
-    render :text => res.body
+    @return = ActiveMerchant::Billing::Integrations::PspPolska::Return.new(res.body)
+    if @return.success?
+      redirect_to @return.redirect_url
+    else
+      raise "Request failed: #{@return.params.inspect} VALID: #{@return.valid?}"
+    end
   end
 
   # handle all returns from psp-polska
